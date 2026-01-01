7a3581497e629261515d97d0c3e898e15fc208a5
Dryzuto <132812992+Dryzuto@users.noreply.github.com>
2026-01-01 18:42:18 +0100

    feat: récupération des posts par auteur et mofication des posts par auteur

--- src/main/java/com/example/accessing_data_mysql/publications/PublicationRepository.java
+++ src/main/java/com/example/accessing_data_mysql/publications/PublicationRepository.java
@@ -3,5 +3,5 @@ package com.example.accessing_data_mysql.publications;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PublicationRepository extends CrudRepository<Publication, Integer> {
-	
+	Iterable<Publication> findByAuthorId(Integer authorId);
 }
